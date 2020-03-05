dee18ad0f0e00f20007d8e02473179dc160d0ce7
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-03-05 16:15:07 +0000

    Add translation models

---
+++ store/translations/models.ts
@@ -0,0 +1,66 @@
+interface SectionTranslations {
+	title: string;
+	description: string;
+}
+
+interface WelcomeSectionTranslations {
+	logoText: string;
+	name: string;
+	grade: string;
+	aboutMe: string;
+	navbar: {
+		skills: string;
+		realizations: string;
+		contact: string;
+		sendMeMessage: string;
+	};
+}
+
+interface SkillsCategoryTranslations {
+	title: string;
+	description: string;
+}
+
+interface SkillsSectionTranslations extends SectionTranslations {
+	frontend: SkillsCategoryTranslations;
+	backend: SkillsCategoryTranslations;
+	embedded: SkillsCategoryTranslations;
+}
+
+interface GithubSectionTranslations extends SectionTranslations {
+	githubRedirectButtonText: string;
+}
+
+interface RealizationsSectionTranslations extends SectionTranslations {
+	project: {
+		detailsButtonText: string;
+		previewButtonText: string;
+		githubButtonText: string;
+	};
+}
+
+interface ContactSectionAboutMeCardTranslations {
+	fullName: string;
+	aboutMe: string;
+}
+
+interface ContactSectionTranslations extends SectionTranslations {
+	aboutMeCard: ContactSectionAboutMeCardTranslations;
+	sendMessageButtonText: string;
+}
+
+interface PrivacyPolicyTranslations {
+	title: string;
+	content: string;
+	acceptButtonText: string;
+	rejectButtonText: string;
+}
+
+export interface Translations {
+	welcome: WelcomeSectionTranslations;
+	skills: SkillsSectionTranslations;
+	github: GithubSectionTranslations;
+	realizations: RealizationsSectionTranslations;
+	contact: ContactSectionTranslations;
+	privacyPolicy: PrivacyPolicyTranslations;
+}
