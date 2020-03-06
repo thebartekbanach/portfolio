6e4f0696f1e033ee0421b581ef6de74f8073d4db
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-03-06 17:27:58 +0000

    Add more availableLanguages reducer tests

--- store/translations/__tests__/reducer.spec.ts
+++ store/translations/__tests__/reducer.spec.ts
@@ -6,7 +6,8 @@ import { translationsReducer } from "../reducer";
 describe("availableLanguages reducer", () => {
 	it("should add missing translations from language/setLanguage/success action payload", () => {
 		const stateMock: AvailableLanguage[] = [
-			{ code: "en", translations: createMock<Translations>() }
+			{ code: "en", name: "English", translations: createMock<Translations>() },
+			{ code: "pl", name: "Polski", translations: undefined }
 		];
 		const polishTranslations = createMock<Translations>();
 
@@ -17,13 +18,29 @@ describe("availableLanguages reducer", () => {
 
 		expect(result).toContainEqual({
 			code: "pl",
+			name: "Polski",
 			translations: polishTranslations
 		});
 	});
 
+	it("should not update state object if translation is alredy fetched", () => {
+		const stateMock: AvailableLanguage[] = [
+			{ code: "en", name: "English", translations: createMock<Translations>() },
+			{ code: "pl", name: "Polski", translations: createMock<Translations>() }
+		];
+
+		const result = translationsReducer.availableLanguages(
+			stateMock,
+			setLanguage.success("pl", createMock<Translations>())
+		);
+
+		expect(result).toBe(stateMock);
+	});
+
 	it("should not change state when language is alredy defined and language/setLanguage/success payload.missingTranslations is undefined", () => {
 		const stateMock: AvailableLanguage[] = [
-			{ code: "en", translations: createMock<Translations>() }
+			{ code: "en", name: "English", translations: createMock<Translations>() },
+			{ code: "pl", name: "Polski", translations: createMock<Translations>() }
 		];
 
 		const result = translationsReducer.availableLanguages(
@@ -31,12 +48,13 @@ describe("availableLanguages reducer", () => {
 			setLanguage.success("en", undefined)
 		);
 
-		expect(result).toBe(stateMock);
+		expect(result).toEqual(stateMock);
 	});
 
 	it("should not change state when language is alredy defined and language/setLanguage/success payload.missingTranslations is null", () => {
 		const stateMock: AvailableLanguage[] = [
-			{ code: "en", translations: createMock<Translations>() }
+			{ code: "en", name: "English", translations: createMock<Translations>() },
+			{ code: "pl", name: "Polski", translations: createMock<Translations>() }
 		];
 
 		const result = translationsReducer.availableLanguages(
