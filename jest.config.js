180c2c54465e987e4f8956cbdab013242ad46fc3
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-03-06 12:03:47 +0000

    Add ts-auto-mock jest plugin

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+require("jest-ts-auto-mock");
+
 const makeModuleNameMapper = require("./tests/rootImports");
 
 module.exports = {
@@ -6,11 +8,12 @@ module.exports = {
 	},
 	globals: {
 		"ts-jest": {
-			tsConfig: "tsconfig.jest.json"
+			tsConfig: "tsconfig.jest.json",
+			compiler: "ttypescript"
 		}
 	},
 	testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
-	testPathIgnorePatterns: ["/lib/", "/node_modules/"],
+	testPathIgnorePatterns: ["/node_modules/", "/.next/"],
 	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 	collectCoverage: true,
 	setupFilesAfterEnv: ["<rootDir>/tests/setupEnzyme.ts", "<rootDir>/tests/setupGlobals.ts"],
