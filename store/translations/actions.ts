7b15fff15e51e1b6b1c23767fcba877dc63f2dc7
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-03-08 16:29:00 +0000

    Update translations actions

--- store/translations/actions.ts
+++ store/translations/actions.ts
@@ -1,20 +1,32 @@
 import { createActionCreator } from "deox";
-import { Translations } from "./models";
+import { PageState } from "./models";
 
 export const setLanguage = {
 	request: createActionCreator(
-		"language/setLanguage/request",
+		"translations/setLanguage/request",
 		resolve => (languageCode: string) => resolve({ languageCode })
 	),
 	success: createActionCreator(
-		"language/setLanguage/success",
-		resolve => (languageCode: string, missingTranslations?: Translations) =>
-			resolve({ languageCode, missingTranslations })
+		"translations/setLanguage/success",
+		resolve => (newLanguageCode: string) => resolve({ newLanguageCode })
 	),
 	failed: createActionCreator(
-		"language/setLanguage/failed",
-		resolve => (languageCode: string, statusCode: number, response?: string) =>
-			resolve({ languageCode }, { statusCode, response })
+		"translations/setLanguage/failed",
+		resolve => (
+			languageCode: string,
+			translationProviderId: string,
+			statusCode: number,
+			response?: string
+		) => resolve({ languageCode }, { translationProviderId, statusCode, response })
 	),
-	pageHideAnimationEnded: createActionCreator("language/setLanguage/pageHideAnimationEnd")
+	pageHasBeenHidden: createActionCreator("translations/setLanguage/pageHasBeenHidden"),
+	translationProviderReady: createActionCreator(
+		"translations/setLanguage/translationProviderReady",
+		resolve => (providerId: string) => resolve({ providerId })
+	)
 };
+
+export const registerTranslationProvider = createActionCreator(
+	"translations/registerTranslationProvider",
+	resolve => (translationProviderId: string) => resolve({ translationProviderId })
+);
