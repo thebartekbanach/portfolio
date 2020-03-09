cda2f0bda3ed3063bdbaddb2b8335fe783285e56
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-03-09 16:57:04 +0000

    Add AvailableLanguage model

--- store/translations/models.ts
+++ store/translations/models.ts
@@ -67,3 +67,5 @@ export interface Translations {
 export type PageState = "shown" | "hiding" | "hidden";
 
 export type LoadingState = { [translationProviderId: string]: boolean };
+
+export type AvailableLanguage = { code: string; name: string };
