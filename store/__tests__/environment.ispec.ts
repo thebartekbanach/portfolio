d36e5e546a23c71bf3e4867b4336fb1e8f0dc284
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-03-11 16:21:14 +0000

    Add integration tests of environment store section

---
+++ store/__tests__/environment.ispec.ts
@@ -0,0 +1,23 @@
+import { configureStore } from "../configureStore";
+import { environment } from "../environment/actions";
+import { isRunningInBrowserSelector } from "../environment/selectors";
+
+describe("environment integration tests", () => {
+	it("should set initial state of isRunningInBrowser to false", () => {
+		const store = configureStore(undefined as any, { isServer: false });
+
+		const result = isRunningInBrowserSelector(store.getState());
+
+		expect(result).toBe(false);
+	});
+
+	it("should change state to true after environment runningEnvironmentChanged action", () => {
+		const store = configureStore(undefined as any, { isServer: false });
+		const action = environment.runningEnvironmentChanged();
+
+		store.dispatch(action);
+		const result = isRunningInBrowserSelector(store.getState());
+
+		expect(result).toBe(true);
+	});
+});
