ff3d159e06747bb1e14f304e050b5b1c2d3ddcb1
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-03-11 15:43:15 +0000

    Add isPageHidden, areAllTranslationsProvidersReady and pendingLanguageCode translations selectors

---
+++ store/translations/__tests__/selectors.spec.ts
@@ -0,0 +1,64 @@
+import { Store } from "~/store";
+import { DeepPartial } from "redux";
+import { isPageHiddenSelector, areAllTranslationProvidersReady } from "../selectors";
+
+describe("translations state selectors", () => {
+	describe("isPageHiddenSelector", () => {
+		it("should return true if languages.pageState is equal to 'hidden'", () => {
+			const state: DeepPartial<Store> = { translations: { pageState: "hidden" } };
+
+			const result = isPageHiddenSelector(state as Store);
+
+			expect(result).toBe(true);
+		});
+
+		it("should return false if languages.pageState is equal to 'hiding'", () => {
+			const state: DeepPartial<Store> = { translations: { pageState: "hiding" } };
+
+			const result = isPageHiddenSelector(state as Store);
+
+			expect(result).toBe(false);
+		});
+
+		it("should return false if languages.pageState is equal to 'shown'", () => {
+			const state: DeepPartial<Store> = { translations: { pageState: "shown" } };
+
+			const result = isPageHiddenSelector(state as Store);
+
+			expect(result).toBe(false);
+		});
+	});
+
+	describe("areAllTranslationProvidersReady", () => {
+		it("should return true if every translation provider is ready", () => {
+			const state: DeepPartial<Store> = {
+				translations: {
+					loadingState: {
+						page: true,
+						contact: true
+					}
+				}
+			};
+
+			const result = areAllTranslationProvidersReady(state as Store);
+
+			expect(result).toBe(true);
+		});
+
+		it("should return false if any translation provider is not ready", () => {
+			const state: DeepPartial<Store> = {
+				translations: {
+					loadingState: {
+						page: true,
+						realizations: false,
+						contact: true
+					}
+				}
+			};
+
+			const result = areAllTranslationProvidersReady(state as Store);
+
+			expect(result).toBe(false);
+		});
+	});
+});
