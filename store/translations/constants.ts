c591913465135191357856055fe6966d2737b386
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-03-18 12:43:07 +0100

    Move translations constats to separate file

---
+++ store/translations/constants.ts
@@ -0,0 +1,2 @@
+export const pageHideTimeoutTime = 2000;
+export const translationProvidersLoadTimeoutTime = 5000;
--- store/translations/index.ts
+++ store/translations/index.ts
@@ -1,7 +1,9 @@
 import * as actions from "./actions";
 import * as selectors from "./selectors";
+import * as constants from "./constants";
 
 export const translations = {
 	actions,
-	selectors
+	selectors,
+	constants
 };
