5adfaa796d74ac8bef3186aa95716e8bacaecae7
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-03-20 11:06:02 +0100

    Add call and race test utilities, modified select test utility

--- tests/utils/__tests__/testEffectsProviders.spec.ts
+++ tests/utils/__tests__/testEffectsProviders.spec.ts
@@ -1,5 +1,6 @@
-import { select } from "../testEffectsProviders";
+import { select, call, race } from "../testEffectsProviders";
 import { translations } from "~/store/translations";
+import * as effects from "~/lib/sagaEffects";
 
 describe("testEffectsProviders", () => {
 	describe("select", () => {
@@ -7,7 +8,7 @@ describe("testEffectsProviders", () => {
 			const mock = jest.fn().mockReturnValue("ok");
 			const selector = translations.selectors.pendingLanguageCode;
 
-			const result = select(selector, mock);
+			const result = select(selector).mockedBy(mock);
 
 			expect(typeof result.select).toEqual("function");
 		});
@@ -16,7 +17,7 @@ describe("testEffectsProviders", () => {
 			const mock = jest.fn().mockReturnValue("ok");
 			const selector = translations.selectors.pendingLanguageCode;
 
-			const provider = select(selector, mock);
+			const provider = select(selector).mockedBy(mock);
 			const result = provider.select({ selector }, () => {});
 
 			expect(result).toEqual("ok");
@@ -29,12 +30,122 @@ describe("testEffectsProviders", () => {
 			const selector = translations.selectors.pendingLanguageCode;
 			const otherSelector = translations.selectors.isPageHidden;
 
-			const provider = select(selector, mock);
+			const provider = select(selector).mockedBy(mock);
 			const result = provider.select({ selector: otherSelector }, next);
 
 			expect(result).toEqual("next value");
 			expect(mock).not.toBeCalled();
 			expect(next).toBeCalled();
+		});
+	});
+
+	describe("call", () => {
+		it("should create correct effect provider", () => {
+			const mock = jest.fn().mockReturnValue("ok");
+			const func = () => "to mock";
+
+			const result = call(func).mockedBy(mock);
+
+			expect(typeof result.call).toEqual("function");
+		});
+
+		it("should return mock() if call is matched", () => {
+			const mock = jest.fn().mockReturnValue("ok");
+			const next = jest.fn();
+			const func = (str: string, num: number) => str + num;
+
+			const caller = call(func, "arg", 1234).mockedBy(mock);
+			const result = caller.call({ fn: func, args: ["arg", 1234] }, next);
+
+			expect(result).toEqual("ok");
+			expect(mock).toBeCalled();
+			expect(next).not.toBeCalled();
+		});
+
+		it("should return next() if call does not match", () => {
+			const mock = jest.fn().mockReturnValue("ok");
+			const next = jest.fn().mockReturnValue("next value");
+			const func = (str: string, num: number) => str + num;
+			const otherFunc = (str: string, num: number) => num + str;
+
+			const provider = call(func, "arg", 1234).mockedBy(mock);
+			const result = provider.call({ fn: otherFunc, args: ["wrong", 5678] }, next);
+
+			expect(result).toEqual("next value");
+			expect(mock).not.toBeCalled();
+			expect(next).toBeCalled();
+		});
+
+		it("should not call mock() if arguments are not equal", () => {
+			const mock = jest.fn().mockReturnValue("ok");
+			const next = jest.fn().mockReturnValue("next value");
+			const func = (str: string, num: number) => str + num;
+
+			const provider = call(func, "arg", 1234).mockedBy(mock);
+			const result = provider.call({ fn: func, args: ["wrong", 5678] }, next);
+
+			expect(result).toEqual("next value");
+			expect(mock).not.toBeCalled();
+			expect(next).toBeCalled();
+		});
+
+		it("should not call mock() if fn is not equal", () => {
+			const mock = jest.fn().mockReturnValue("ok");
+			const next = jest.fn().mockReturnValue("next value");
+			const func = (str: string, num: number) => str + num;
+			const otherFunc = (str: string, num: number) => num + str;
+
+			const provider = call(func, "arg", 1234).mockedBy(mock);
+			const result = provider.call({ fn: otherFunc, args: ["arg", 1234] }, next);
+
+			expect(result).toEqual("next value");
+			expect(mock).not.toBeCalled();
+			expect(next).toBeCalled();
+		});
+	});
+
+	describe("race", () => {
+		it("should call mock() if parameters are equal", () => {
+			const mock = jest.fn().mockReturnValue("ok");
+			const next = jest.fn().mockReturnValue("next value");
+			const fakeApi = jest.fn().mockReturnValue("fakeApi");
+			const params = {
+				value: effects.race.call(fakeApi),
+				timeout: effects.delay(100)
+			};
+			const testParams = {
+				value: effects.race.call(fakeApi),
+				timeout: effects.delay(100)
+			};
+
+			const provider = race(params).mockedBy(mock);
+			const result = provider.race(testParams, next);
+
+			expect(result).toEqual("ok");
+			expect(mock).toBeCalled();
+			expect(next).not.toBeCalled();
+		});
+
+		it("should call next() if parameters are not equal", () => {
+			const mock = jest.fn().mockReturnValue("ok");
+			const next = jest.fn().mockReturnValue("next value");
+			const fakeApi = jest.fn().mockReturnValue("fakeApi");
+			const otherApi = jest.fn().mockReturnValue("otherApi");
+			const params = {
+				value: effects.race.call(fakeApi),
+				timeout: effects.delay(100)
+			};
+			const testParams = {
+				value: effects.race.call(otherApi),
+				timeout: effects.delay(5000)
+			};
+
+			const provider = race(params).mockedBy(mock);
+			const result = provider.race(testParams, next);
+
+			expect(result).toEqual("next value");
+			expect(next).toBeCalled();
+			expect(mock).not.toBeCalled();
 		});
 	});
 });
--- tests/utils/testEffectsProviders.ts
+++ tests/utils/testEffectsProviders.ts
@@ -1,11 +1,43 @@
 import { Store } from "~/store";
+import deepEqual from "deep-equal";
 
-export const select = <T>(selectorFunc: (store: Store) => T, mock: jest.Mock<T>) => ({
-	select({ selector }: { selector: Function }, next: () => any) {
-		if (selectorFunc === selector) {
-			return mock();
+export const select = (selectorFunc: (store: Store) => unknown) => ({
+	mockedBy: (mock: jest.Mock) => ({
+		select({ selector }: { selector: Function }, next: Function) {
+			if (selectorFunc === selector) {
+				return mock();
+			}
+
+			return next();
 		}
+	})
+});
 
-		return next();
-	}
+type Parameters<T> = T extends (...args: infer T) => any ? T : never;
+
+export const call = <Fn extends (...args: any[]) => any>(
+	callFunc: Fn,
+	...params: Parameters<Fn>
+) => ({
+	mockedBy: (mock: jest.Mock) => ({
+		call(effect: { fn: Function; args: any[] }, next: Function) {
+			if (effect.fn === callFunc && deepEqual(effect.args, params)) {
+				return mock();
+			}
+
+			return next();
+		}
+	})
+});
+
+export const race = <T>(raceConfig: T) => ({
+	mockedBy: (mock: jest.Mock) => ({
+		race(raceCallParams: T | any, next: Function) {
+			if (deepEqual(raceCallParams, raceConfig)) {
+				return mock();
+			}
+
+			return next();
+		}
+	})
 });
