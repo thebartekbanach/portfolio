4363a70658e957bb3e59fec6a413fdebaa8f3c47
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-04-03 10:56:12 +0200

    Change locales directory

--- lib/i18next.ts
+++ lib/i18next.ts
@@ -3,7 +3,8 @@ import NextI18Next from "next-i18next";
 const i18nextInstance = new NextI18Next({
 	defaultLanguage: "en",
 	otherLanguages: ["pl"],
-	defaultNS: "common"
+	defaultNS: "common",
+	localePath: "public/locales"
 });
 
 export default i18nextInstance;
