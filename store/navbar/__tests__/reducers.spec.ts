d52c4354f4f3c44f05b75e26a630739d1b521a2a
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-04-08 14:11:30 +0200

    Fix navbar reducers exports

--- store/navbar/__tests__/reducers.spec.ts
+++ store/navbar/__tests__/reducers.spec.ts
@@ -1,12 +1,12 @@
-import { mobileNavbarOpenState } from "../reducers";
+import { navbarReducers } from "../reducers";
 import { navbar } from "..";
 
-describe(mobileNavbarOpenState, () => {
+describe(navbarReducers.mobileNavbarOpenState, () => {
 	it("should toggle state to true", () => {
 		const state = false;
 		const action = navbar.actions.internal.setMenuState(true);
 
-		const result = mobileNavbarOpenState(state, action);
+		const result = navbarReducers.mobileNavbarOpenState(state, action);
 
 		expect(result).toBe(true);
 	});
@@ -15,7 +15,7 @@ describe(mobileNavbarOpenState, () => {
 		const state = true;
 		const action = navbar.actions.internal.setMenuState(false);
 
-		const result = mobileNavbarOpenState(state, action);
+		const result = navbarReducers.mobileNavbarOpenState(state, action);
 
 		expect(result).toBe(false);
 	});
--- store/navbar/reducers.ts
+++ store/navbar/reducers.ts
@@ -2,10 +2,12 @@ import { createReducer } from "deox";
 import { combineReducers } from "redux";
 import { navbar } from ".";
 
-export const mobileNavbarOpenState = createReducer(false, handle => [
+const mobileNavbarOpenState = createReducer(false, handle => [
 	handle(navbar.actions.internal.setMenuState, (_, action) => action.payload.nextMenuState)
 ]);
 
-export default combineReducers({
+export const navbarReducers = {
 	mobileNavbarOpenState
-});
+};
+
+export default combineReducers(navbarReducers);
