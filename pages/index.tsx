684f5747cea0a848e7cbc3674d08106122142fb9
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-04-11 17:23:14 +0200

    Fix compilation errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,5 @@
-import { HelloWorldComponent } from "../components/helloWorld";
-import { SomeContainer } from "~/containers/SomeContainer";
 import { NextPage } from "next";
-import { Navbar } from "~/containers/Navbar";
 import { Main } from "~/components/shared/layout/main";
-import { Section } from "~/components/shared/layout/section";
 import { WelcomeSection } from "~/containers/sections/welcomeSection";
 
 const Index: NextPage = () => (
