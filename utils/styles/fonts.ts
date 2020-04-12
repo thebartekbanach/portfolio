824ef94ddc77b77cacb1ac5b9bf4433a497eb524
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-04-12 12:32:03 +0200

    Add montserrat extra bold font

--- utils/styles/fonts.ts
+++ utils/styles/fonts.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from "styled-components";
 
 export const FontsImport = createGlobalStyle`
+	/* nunito-sans */
+
 	@font-face {
 		font-family: nunito-sans-regular;
 		src:
@@ -32,6 +34,16 @@ export const FontsImport = createGlobalStyle`
 			url("fonts/nunito-sans/extra-bold/font.woff") format("woff"),
 			url("fonts/nunito-sans/extra-bold/font.woff2") format("woff2");
 	}
+
+	/* montserrat */
+
+	@font-face {
+		font-family: montserrat-extra-bold;
+		src:
+			url("fonts/montserrat/extra-bold/font.ttf") format("truetype"),
+			url("fonts/montserrat/extra-bold/font.woff") format("woff"),
+			url("fonts/montserrat/extra-bold/font.woff2") format("woff2");
+	}
 `;
 
 export const useFont = {
@@ -40,5 +52,8 @@ export const useFont = {
 		semiBold: "font-family: nunito-sans-semi-bold, sans-serif; font-weight: 500;",
 		bold: "font-family: nunito-sans-bold, sans-serif; font-weight: 600;",
 		extraBold: "font-family: nunito-sans-extra-bold, sans-serif; font-weight: 800;"
+	},
+	montserrat: {
+		extraBold: "font-family: montserrat-extra-bold, sans-serif; font-weight: 800;"
 	}
 };
