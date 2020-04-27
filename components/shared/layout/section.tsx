e4dcff1c19aa9cabf2250087c65d1c618151ef39
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-04-27 13:40:37 +0200

    Add isFirstSection attribute to Section component

--- components/shared/layout/section.tsx
+++ components/shared/layout/section.tsx
@@ -1,6 +1,12 @@
 import styled from "styled-components";
 
-export const Section = styled.section`
+interface SectionProps {
+	isFirstSection?: boolean;
+}
+
+export const Section = styled.section<SectionProps>`
 	display: block;
 	width: 100%;
+
+	padding-top: ${p => (p.isFirstSection ? 0 : 100)}px;
 `;
