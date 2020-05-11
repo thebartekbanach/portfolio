17a6d63bef488443241b61b8df14d3abd754a26b
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-05-11 15:53:06 +0200

    Fix skills board positioning

--- containers/sections/skillsSection/index.tsx
+++ containers/sections/skillsSection/index.tsx
@@ -4,14 +4,17 @@ import { useTranslation } from "~/utils/i18next";
 import { CategoryPicker } from "./categoryPicker";
 import { SkillsBoard } from "./skillsBoard";
 import { SkillsGrid } from "~/components/sections/skillsSection/skillTiles";
+import { useSelector } from "react-redux";
+import { skills } from "~/store/skills";
 
 export const SkillsSection = () => {
 	const { t } = useTranslation("skills");
+	const selectedCategoryName = useSelector(skills.selectors.selectedCategoryName);
 
 	return (
 		<Section>
 			<SectionHeaders title={t("sectionTitle")} description={t("sectionDescription")} />
-			<SkillsGrid activeCategoryName="frontend">
+			<SkillsGrid activeCategoryName={selectedCategoryName || "none"}>
 				<CategoryPicker />
 				<SkillsBoard />
 			</SkillsGrid>
