f2cbdd16c7eb61b03160912b86feff1ed57d51e4
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-05-11 15:46:20 +0200

    Add skillSectionSetup in getInitialProps for index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { NextPage } from "next";
 import { Main } from "~/components/shared/layout/main";
 import { WelcomeSection } from "~/containers/sections/welcomeSection";
 import { SkillsSection } from "~/containers/sections/skillsSection";
+import { dispatchAndWaitForResult } from "~/utils/dispatchAndWaitForResult";
+import { skills } from "~/store/skills";
 
 const Index: NextPage = () => (
 	<Main>
@@ -9,5 +11,15 @@ const Index: NextPage = () => (
 		<SkillsSection />
 	</Main>
 );
+
+Index.getInitialProps = async ctx => {
+	await dispatchAndWaitForResult(
+		ctx.store,
+		skills.actions.setupSkillsSection(),
+		skills.selectors.areBoardsContentsInitialized
+	);
+
+	return {};
+};
 
 export default Index;
