c3a8a5df4366b84ee7c61fa47ec05384a739dba1
Bartek Banach <thebartekbanach.dev@gmail.com>
2020-05-11 15:45:34 +0200

    Add saga context initialization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "normalize.css";
 import Head from "next/head";
-import App, { AppContext } from "next/app";
+import App from "next/app";
 import withRedux from "next-redux-wrapper";
 import withReduxSaga from "next-redux-saga";
 import { configureStore } from "~/store/configureStore";
@@ -8,14 +8,48 @@ import { Provider } from "react-redux";
 import { appWithTranslation } from "~/utils/i18next";
 import { GlobalStyles } from "~/utils/styles/globals";
 import { Store } from "redux";
+import { dispatchAndWaitForResult } from "~/utils/dispatchAndWaitForResult";
+import { language } from "~/store/language";
+import { channel } from "redux-saga";
+import { MyAppContext } from "~/utils/applicationModels";
 
 interface MyAppProps {
 	store: Store;
 }
 
+export function initializeSagaContext(ctx: MyAppContext["ctx"]) {
+	const { i18n } = ctx.req;
+
+	const i18nLanguageChangeChannel = channel<string>();
+	i18n.on("languageChanged", i18nLanguageChangeChannel.put);
+
+	return {
+		i18n,
+		i18nLanguageChangeChannel
+	};
+}
+
+async function bootstrapApplication(ctx: MyAppContext["ctx"]) {
+	if (ctx.isServer) {
+		if (!ctx.req.language) {
+			ctx.req.language = "en";
+		}
+
+		await dispatchAndWaitForResult(
+			ctx.store,
+			language.actions.setupLanguageOnServerSide(ctx.req.language || "en"),
+			state => language.selectors.currentLanguage(state) === ctx.req.language
+		);
+	}
+}
+
 class MyApp extends App<MyAppProps> {
-	static async getInitialProps({ Component, ctx }: AppContext) {
+	static async getInitialProps({ Component, ctx }: MyAppContext) {
 		let pageProps = {};
+
+		const sagaContext = initializeSagaContext(ctx);
+		ctx.store.sagaTask.setContext(sagaContext);
+		await bootstrapApplication(ctx);
 
 		if (Component.getInitialProps) {
 			pageProps = await Component.getInitialProps(ctx);
