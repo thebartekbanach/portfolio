33fedd288b8fdd9a3ba7d4e53248f2e664a0e5f4
Bartłomiej Banach <thebartekbanach.dev@gmail.com>
2020-11-04 15:42:23 +0100

    refactor: fix svg file import type

--- src/next-env.d.ts
+++ src/next-env.d.ts
@@ -2,7 +2,6 @@
 /// <reference types="next/types/global" />
 
 declare module "*.svg" {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const image: any;
-	export default image;
+	const svg: React.ElementType;
+	export default svg;
 }
