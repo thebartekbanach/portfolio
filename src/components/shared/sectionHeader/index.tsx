0048b9b73f5a3c4197720e50b139e8bb541f67b1
Bartłomiej Banach <thebartekbanach.dev@gmail.com>
2020-11-06 22:11:52 +0100

    feat: add section header shared component

---
+++ src/components/shared/sectionHeader/index.tsx
@@ -0,0 +1,18 @@
+import { FC } from "react";
+import {
+	SectionHeaderElement,
+	SectionDescription,
+	SectionHeaderAndDescriptionWrapper
+} from "./styles";
+
+interface SectionHeaderProps {
+	sectionName: string;
+	description: string;
+}
+
+export const SectionHeader: FC<SectionHeaderProps> = ({ sectionName, description }) => (
+	<SectionHeaderAndDescriptionWrapper>
+		<SectionHeaderElement>{sectionName}</SectionHeaderElement>
+		<SectionDescription>{description}</SectionDescription>
+	</SectionHeaderAndDescriptionWrapper>
+);
---
+++ src/components/shared/sectionHeader/styles.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { useFont } from "~/utils/useFont";
+
+export const SectionHeaderAndDescriptionWrapper = styled.header`
+	text-align: center;
+	margin-top: 160px;
+`;
+
+export const SectionHeaderElement = styled.h2`
+	${useFont.montserrat.extraBold};
+	font-size: 40px;
+	color: #4764e6;
+`;
+
+export const SectionDescription = styled.span`
+	display: inline-block;
+	max-width: 250px;
+
+	${useFont.nunitoSans.regular};
+	font-size: 17px;
+	letter-spacing: 0.7px;
+	line-height: 1.2em;
+	color: #909090;
+`;
