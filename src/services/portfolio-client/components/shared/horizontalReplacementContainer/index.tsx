5677830733f97b865a7bb3b887ed1eb5e4c04f18
Bartłomiej Banach <thebartekbanach.dev@gmail.com>
2021-01-02 17:39:41 +0100

    feat(portfolio-client): add HorizontalReplacementContainer component

---
+++ src/services/portfolio-client/components/shared/horizontalReplacementContainer/index.tsx
@@ -0,0 +1,69 @@
+import { FC, isValidElement, useEffect, useRef, useState } from "react";
+
+import { AnimateWidth } from "../animateWidth";
+
+import { CurrentChildrenWrapper, NextChildrenAbsoluteWrapper } from "./styles";
+
+interface HorizontalReplacementContainerProps {
+	zIndex?: number;
+}
+
+export const HorizontalReplacementContainer: FC<HorizontalReplacementContainerProps> = ({
+	children,
+	zIndex
+}) => {
+	if (!isValidElement(children)) {
+		throw new Error("ReplacementContainer children must be a valid react element");
+	}
+
+	if (children.key === null || children.key === undefined) {
+		throw new Error("ReplacementContainer children key cannot be null or undefined");
+	}
+
+	const [isChildrenVisible, setIsChildrenVisible] = useState(true);
+	const [currentChildren, setCurrentChildren] = useState(children);
+	const [widthOverride, setWidthOverride] = useState<"auto" | number>("auto");
+
+	const nextChildrenRef = useRef<HTMLDivElement>(null);
+
+	const onWidthAnimationEnd = () => {
+		if (!isChildrenVisible) {
+			setCurrentChildren(children);
+			setIsChildrenVisible(true);
+			setWidthOverride("auto");
+		}
+	};
+
+	useEffect(() => {
+		if (nextChildrenRef.current === null) {
+			return;
+		}
+
+		const nextChildrenWidth = nextChildrenRef.current.offsetWidth;
+		setWidthOverride(nextChildrenWidth);
+		setIsChildrenVisible(false);
+	}, [children.key]);
+
+	useEffect(() => {
+		// update current children when props have been changed
+		if (currentChildren.key === children.key) {
+			setCurrentChildren(children);
+		}
+	}, [children.props]);
+
+	const nextChildrenAbsoluteWrapper =
+		currentChildren.key === children.key ? null : (
+			<NextChildrenAbsoluteWrapper ref={nextChildrenRef}>
+				{children}
+			</NextChildrenAbsoluteWrapper>
+		);
+
+	return (
+		<AnimateWidth duration={500} width={widthOverride} onAnimationEnd={onWidthAnimationEnd}>
+			<CurrentChildrenWrapper isContentVisible={isChildrenVisible} zIndex={zIndex}>
+				{currentChildren}
+			</CurrentChildrenWrapper>
+			{nextChildrenAbsoluteWrapper}
+		</AnimateWidth>
+	);
+};
---
+++ src/services/portfolio-client/components/shared/horizontalReplacementContainer/styles.tsx
@@ -0,0 +1,30 @@
+import styled, { css } from "styled-components";
+
+export const NextChildrenAbsoluteWrapper = styled.div`
+	position: absolute;
+
+	top: 0;
+	left: 0;
+	width: auto;
+	height: auto;
+
+	opacity: 0;
+`;
+
+interface CurrentChildrenWrapperProps {
+	isContentVisible: boolean;
+	zIndex?: number;
+}
+
+export const CurrentChildrenWrapper = styled.div<CurrentChildrenWrapperProps>`
+	${p =>
+		p.zIndex === undefined
+			? null
+			: css`
+					position: relative;
+					z-index: ${p.zIndex};
+			  `}
+
+	opacity: ${p => (p.isContentVisible ? 1 : 0)};
+	transition: opacity 300ms;
+`;
