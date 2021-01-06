fc88779600b3f19ea6883be319736505514ecca1
Bartłomiej Banach <thebartekbanach.dev@gmail.com>
2021-01-06 18:29:34 +0100

    fix(portfolio-client/styles): add missing hover animation

--- src/services/portfolio-client/components/pages/index/sections/welcome/mobile/styles.tsx
+++ src/services/portfolio-client/components/pages/index/sections/welcome/mobile/styles.tsx
@@ -43,10 +43,18 @@ export const FindOutMoreButton = styled.a`
 
 	padding: 20px 50px;
 
+	box-shadow: 1.5px 1.4px 8px 0 rgba(94, 88, 248, 0.46);
 	background: #5f55f7;
 	border-radius: 5px;
+
+	transition: transform 300ms, box-shadow 250ms;
 
 	${useFont.nunitoSans.regular};
 	text-decoration: none;
 	color: white;
+
+	&:hover {
+		transform: scale(0.95);
+		box-shadow: 0px 0px 0px 0 rgba(94, 88, 248, 0.46);
+	}
 `;
