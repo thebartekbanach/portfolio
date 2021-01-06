1662098111fe4350a3dd5ca9dd70bccada0204c3
Bartłomiej Banach <thebartekbanach.dev@gmail.com>
2021-01-06 18:25:46 +0100

    fix(portfolio-client/styles): missing gradient on desktop welcome section

--- src/services/portfolio-client/components/pages/index/sections/welcome/desktop/index.tsx
+++ src/services/portfolio-client/components/pages/index/sections/welcome/desktop/index.tsx
@@ -17,7 +17,7 @@ export const TitleWithGradient: FC = ({ children }) => (
 	<svg width="100%" viewBox="0 0 222 23">
 		<defs>
 			<linearGradient
-				id="welcomeSectionTitleGradientSetup"
+				id="myNameTextLinearGradientDef"
 				x1="0"
 				x2="0"
 				y1="0"
@@ -28,7 +28,7 @@ export const TitleWithGradient: FC = ({ children }) => (
 				<stop stopColor="#694fff" offset="100%" />
 			</linearGradient>
 		</defs>
-		<text x="-2.1" y="22.8" fill="url(#welcomeSectionTitleGradientSetup)">
+		<text x="-2.1" y="22.8" fill="url(#myNameTextLinearGradientDef)">
 			{children}
 		</text>
 	</svg>
